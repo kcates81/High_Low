3ccf1414cb3ff0e1849de03c83d5270e742c9c2e
kcates81 <kriscates81@gmail.com>
2016-04-15 14:22:11 -0500

    added do/while loops to my if/else statements so that the user can keep guessing until they get guess the random number

--- high-low.php
+++ high-low.php
@@ -9,3 +9,23 @@ echo "Guess a number between 1 and 100\n";
 $userGuess = fgets(STDIN);
 $userGuess = trim($userGuess);
 
+if ($userGuess == $randomNum) {
+    echo "GOOD GUESS!\n";
+} elseif ($userGuess > $randomNum) {
+    
+    echo "Too high. Try again...\n";
+    
+    do {
+        $userGuess = fgets(STDIN);
+        $userGuess = trim($userGuess);
+    } while ($userGuess > $randomNum);
+    
+} else {
+    echo "Too low. Try again...\n";
+
+    do {
+        $userGuess = fgets(STDIN);
+        $userGuess = trim($userGuess);
+    } while ($userGuess < $randomNum);
+    
+}
