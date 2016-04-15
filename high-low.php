3c0637a3e1a2d9a5fae28bddc0c7ab527bf953bb
kcates81 <kriscates81@gmail.com>
2016-04-15 14:56:50 -0500

    got rid of multiple do/while statements within my if/elseif/else statement. now have 1 do while that runs with the user's guess doesn't equal the random number generated by the game. within the do/while loop, there is an if/elseif/else statement that generates messages and/or gives the user another turn until they guess the right number

--- high-low.php
+++ high-low.php
@@ -4,28 +4,25 @@ $randomNum = mt_rand(1, 100);
 
 var_dump($randomNum);
 
-echo "Guess a number between 1 and 100\n";
+do {
+    echo "Guess a number between 1 and 100\n";
 
-$userGuess = fgets(STDIN);
-$userGuess = trim($userGuess);
+    $userGuess = fgets(STDIN);
+    $userGuess = trim($userGuess);
 
-if ($userGuess == $randomNum) {
-    echo "GOOD GUESS!\n";
-} elseif ($userGuess > $randomNum) {
-    
-    echo "Too high. Try again...\n";
-    
-    do {
-        $userGuess = fgets(STDIN);
-        $userGuess = trim($userGuess);
-    } while ($userGuess > $randomNum);
-    
-} else {
-    echo "Too low. Try again...\n";
+    if ($userGuess == $randomNum) {
+        echo "GOOD GUESS!\n";
+    } elseif ($userGuess > $randomNum) {
 
-    do {
-        $userGuess = fgets(STDIN);
-        $userGuess = trim($userGuess);
-    } while ($userGuess < $randomNum);
-    
-}
+        echo "Too high. Try again...\n";
+
+    } else {
+
+        echo "Too low. Try again...\n";
+        
+    }
+} while ($userGuess != $randomNum);
+
+
+
+
